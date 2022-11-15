c7ed000473d6070cdb569fe41a50079df1cfd69c
vikiyev <demiglace0505@gmail.com>
2022-11-15 14:38:36 +0800

    bootcamps routes

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
+
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+// create routes
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: { message: "Hello" } });
+});
+
+
+
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
